51bb450033ad9064bd26ae03b507faa0f20ce216
Jeju-Boobby <boo1984@naver.com>
2017-12-12 17:18:12 +0900

    edit

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,26 @@ var ghPages = require('gulp-gh-pages');
 var cleanCSS = require('gulp-clean-css');
 var livereload = require('gulp-livereload');
 
-var js = ['/src/js/*.js'];
-var css = ['/src/css/*.css'];
-var html = ['/src/**/*.html'];
+var js = ['src/js/*.js'];
+var css = ['src/css/*.css'];
+var html = ['src/**/*.html'];
 
 gulp.task('uglify-js', function() {
     return gulp.src(js)
             .pipe(uglify())
-            .pipe(gulp.dest('/dist/js/script.js'))
+            .pipe(gulp.dest('dist/js/script.js'))
 });
 
 gulp.task('compress-html', function () {
 	return gulp.src(html)
   .pipe(minifyhtml())
-		.pipe(gulp.dest('/dist/'));
+		.pipe(gulp.dest('dist/'));
 });
 
 gulp.task('minify-css', function () {
   return gulp.src(css)
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('/dist/css/'));
+    .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('watch', function () {
@@ -32,10 +32,10 @@ gulp.task('watch', function () {
 	gulp.watch(js, ['uglify-js']);
 	gulp.watch(css, ['minify-css']);
 	gulp.watch(html, ['compress-html']);
-	gulp.watch('/dist/**').on('change', livereload.changed);
+	gulp.watch('dist/**').on('change', livereload.changed);
 });
 
 gulp.task('deploy', ['uglify-js', 'compress-html', 'minify-css', 'watch'], function() {
-  return gulp.src('/dist/**/*')
+  return gulp.src('dist/**/*')
     .pipe(ghPages({branch: 'gh-pages'}));
 });
