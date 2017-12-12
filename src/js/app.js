8f63068b00dbea8152d8e14c3e42c653433660b8
Jeju-Boobby <boo1984@naver.com>
2017-12-12 18:46:43 +0900

    file save

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
-console.log('my-note app.js');
+// console.log('my-note app.js');
+// require
+var FileSaver = require('file-saver');
+
+//DOM
+var memo = document.getElementById('memo');
+var btn_savenote = document.getElementById('btn-savenote');
+
+var saveNote = function () {
+  // console.log(memo.value);
+  var blob = new Blob([memo.value], {type: "text/plain;charset=utf-8"});
+  FileSaver.saveAs(blob, "MyNote.txt");
+}
+
+btn_savenote.addEventListener('click', saveNote);
